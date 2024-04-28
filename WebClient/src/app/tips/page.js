9a1af25e6657a64e5122a779c40488ce739473e9
iho21 <hoivan08@gmail.com>
2024-04-28 00:43:28 -0700

    Stretched banner

--- WebClient/src/app/tips/page.js
+++ WebClient/src/app/tips/page.js
@@ -2,30 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1 className="relative h-32 w-32 basis-1/2 bg-purple-200 top-0 text-center self-stretch align-top">
-          Plastic
-          
-        </h1>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main className="flex flex-col items-center justify-between p-24">
+      <div className="z-10 w-full items-center justify-between font-mono== lg:flex">
+        <h1 className="relative h-32 w-full bg-purple-200 top-0 text-center self-stretch align-top">
+          Plastic</h1>
+        
       </div>
 
 
